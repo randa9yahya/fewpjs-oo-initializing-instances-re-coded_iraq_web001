8ded67ecb7a08eeb683e829f729cc244bf3f3a3d
randa yahya <randaalrayan96@gmial.com>
2020-06-15 14:10:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code here
+class Breakfast {
+  constructor(food,drink) {
+    this.food = food;
+    this.drink =drink;
+  }
+}
+  bfast = new Breakfast('eggs', 'juice');
+
+class Lunch {
+  constructor(salad,soup,drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+  newlunch = new Lunch('side salad', 'broccoli cheddar soup', 'iced tea');
+
+class Dinner  {
+  constructor(salad,soup,entree,dessert) {
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this.dessert = dessert;
+  }
+}
+  newdinner = new Dinner('balsamic salad', 'consomme', 'filet mignon', 'cheesecake')
